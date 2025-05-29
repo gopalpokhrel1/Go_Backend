dbce982948e6427707601c6a85aad8b1b61e8b56
gopalpokhrel <gopalpokhrel101@gmail.com>
2025-05-30 00:03:55 +0545

    add request validation

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	"github.com/gopalpokhrel1/students-api/internal/config"
+	student "github.com/gopalpokhrel1/students-api/internal/http/handlers/students"
 )
 
 func main() {
@@ -20,9 +21,7 @@ func main() {
 	//setup router
 	router := http.NewServeMux()
 
-	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Welcome to students api"))
-	})
+	router.HandleFunc("POST /api/students", student.New())
 
 	//setup server
 	server := http.Server{
